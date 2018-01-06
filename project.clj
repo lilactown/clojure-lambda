df75e608cdfeed809a134e4fb9bb0abc4ff8586d
Will Acton <will@willacton.com>
2018-01-05 16:04:19 -0800

    deflambda macro & middleware start

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.amazonaws/aws-lambda-java-core "1.1.0"]
                  [com.amazonaws/aws-lambda-java-events "2.0.1"]
+                 [lambada "1.0.2"]
                  [org.clojure/data.json "0.2.6"]
                  [compojure "1.6.0"]]
   :source-paths ["src"]
